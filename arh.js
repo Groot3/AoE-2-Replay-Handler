87df0f5f0cb0f005f7d538538b573fd7c9dba11b
Joe H <stik0174@gmail.com>
2021-01-13 01:51:17 -0500

    rerun/read dirs without relaunch / code cleanup
    1.1.0 - Fixed issue where program wouldn't read changes to savegame folder until relaunching the program.
    
    Better documentation & code cleanup

--- arh.js
+++ arh.js
@@ -1,66 +1,74 @@
 // Age Of Empires 2 Replay Handler (ARH)
 // 1/13/2021
 // Groot
-// 1.0.0
+// 1.1.0
 
 const fs = require('fs')
 const keypress = require('keypress')
 const unzipper = require('unzipper')
-const origin = ''// Your default download folder for your browser
+const downloads = ''// Your default download folder for your browser
 //i.e `C:\\Users\\(YOUR NAME)\\Downloads\\`
-const goto = ''// Your AoE replay directory here. 
+const saveGame = ''// Your AoE replay directory here. 
 // i.e 'C:\\Users\\(YOUR NAME)\\Games\\Age of Empires 2 DE\\(NUMBERS)\\savegame\\'
-const fileorigin = fs.readdirSync(origin)
-const filegoto = fs.readdirSync(goto)
 
-let count = 0
 const replayFiles = []
 const unzipFiles = []
 
 keypress(process.stdin)
 
-main()
+let running = false
+while (running == false) {
+    running = true
+    main(running)
+}
 
-function main() {
+function main(running) {
+    let unzipCount = 0
+    let moveCount = 0
+
+    const downloadsFile = fs.readdirSync(downloads) 
+    const saveGameFile = fs.readdirSync(saveGame)
+
     console.log("Press F7 to move zip filves to the savegame folder")
     console.log("Press F8 to unzip AoE files in the savegame folder")
-    //console.log("Press F9 to buffer files (required inbetween steps)")
     process.stdin.on('keypress', (ch,key) => {
         if (key.name == "f7") { // Move files to AoE folder
-            for (file of fileorigin) {
-                if (file.includes('AgeIIDE_Replay')) {
+            for (file of downloadsFile) {
+                if (file.includes('AgeIIDE_Replay')) { // Checks if file in downloads
+                    // is default aoe2.net filename.
                     console.log(file)
-                    replayFiles.push(file)
+                    replayFiles.push(file) // Stores fitting files in replayFiles
                 }
             }
     
-            for (file of replayFiles) {
-                const path = origin+file
-                //console.log("Path is:",path)
-                fs.rename(path,(goto+file), (error) => {
+            for (file of replayFiles) { 
+                const path = downloads+file // Set path to be read
+                fs.rename(path,(saveGame+file), (error) => { // Rename each replayFile in path
                     if (error) throw error
-                    count = count + 1
-                    console.log(count +" file/s moved." + " " + path)
+                    moveCount = moveCount + 1
+                    console.log(moveCount +" file/s moved." + " " + path)
                 })
-            }
-            return
+            } 
+            //TODO: make this show up after files are moved.
+            console.log(`Finished moving files.`)
+            return main()
         }
         if (key.name == "f8") { // Unzip files in AoE folder
-            for (file of filegoto) {
-                console.log(file)
-                if (file.includes('.zip')) {
-                    unzipFiles.push(file)
+            for (file of saveGameFile) { 
+                if (file.includes('.zip')) { // Checks for zip files in AoE savegame folder
+                    //console.log(file)
+                    unzipFiles.push(file) // Adds them to unzipFiles array
                 }
-            for (file of unzipFiles) {
-                const path = goto+file
-                count = count + 1
-                fs.createReadStream(path)
-                    .pipe(unzipper.Extract({path:goto}))
-                console.log("Processing files.." + count)
+            for (file of unzipFiles) { // Each .zip file
+                const path = saveGame+file // Set path to be read
+                unzipCount = unzipCount + 1
+                fs.createReadStream(path) // Reads path of files containing .zip
+                    .pipe(unzipper.Extract({path:saveGame})) // Unzips them to savegame folder
+                console.log("Processing files.." + unzipCount)
             }
             }
             console.log("Files Unzipped.")
-            return
+            return main()
         }
     })
     
